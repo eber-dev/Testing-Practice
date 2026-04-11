b5aa2c9155e1eb91380e408077dd624697cd5cc8
Eber <www.eberacunaquispe@gmail.com>
2026-04-11 11:38:09 -0500

    Se escribio los test de las funciones

---
+++ src/pruebas.test.js
@@ -0,0 +1,35 @@
+import {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    analyzeArray,
+} from './index.js';
+
+test('capitalizar', () => {
+    expect(capitalize('hola')).toBe('Hola');
+});
+
+test('alreves', () => {
+    expect(reverseString('arbol').tobe('lobra'));
+});
+
+test('calculadora', () => {
+    expect(calculator.add(2, 1)).toBe(3);
+    expect(calculator.sub(5, 1)).toBe(4);
+    expect(calculator.multiply(1, 1)).toBe(1);
+    expect(calculator.divide(6, 1)).toBe(6);
+});
+
+test('caesarCipher', () => {
+    expect(caesarCipher('abc', 3)).toBe('def');
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+    expect(caesarCipher('HeLLLo', 3)).toBe('KhOOOr');
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
+
+test('analyzeArray', () => {
+    const input = [1, 8, 3, 4, 2, 6];
+    const expectedValue = { average: 4, min: 1, max: 8, length: 6 };
+    expect(analyzeArray(input)).toEqual(expectedValue);
+});
