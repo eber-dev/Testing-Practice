6d09def3c4cd09d60f0d706029e340c6cf697ecf
Eber <www.eberacunaquispe@gmail.com>
2026-04-11 11:09:20 -0500

    Se añadieron las funciones que requiere el ejercicio

--- src/index.js
+++ src/index.js
@@ -14,8 +14,96 @@ export function reverseString(string) {
     return final;
 }
 
-export function calculator() {}
+export function calculator(num1, num2, operator) {
+    let resultado;
+    switch (operator) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            resultado = num1 / num2;
+            break;
+        default:
+            alert('Operador incorrecto');
+    }
+    return resultado;
+}
 
-export function caesarCipher() {}
+export function caesarCipher(cadena, desplazamiento) {
+    let resultado = '';
 
-export function analyzeArray() {}
+    for (let i = 0; i < cadena.length; i++) {
+        let letra = cadena[i];
+
+        // Convertir letra → número
+        let codigo = letra.charCodeAt(0);
+
+        // MAYÚSCULAS
+        if (codigo >= 65 && codigo <= 90) {
+            let nuevaLetra = String.fromCharCode(
+                ((((codigo - 65 + desplazamiento) % 26) + 26) % 26) + 65,
+            );
+            resultado += nuevaLetra;
+
+            // minúsculas
+        } else if (codigo >= 97 && codigo <= 122) {
+            let nuevaLetra = String.fromCharCode(
+                ((((codigo - 97 + desplazamiento) % 26) + 26) % 26) + 97,
+            );
+            resultado += nuevaLetra;
+
+            // otros caracteres (espacios, símbolos)
+        } else {
+            resultado += letra;
+        }
+    }
+
+    return resultado;
+}
+
+export function analyzeArray(array) {
+    function average(array) {
+        let acumulado = 0;
+        for (let i = 0; i < array.length; i++) {
+            acumulado += array[i];
+        }
+
+        return acumulado / array.length;
+    }
+
+    function min(array) {
+        let menor = array[0];
+
+        for (let i = 0; i < array.length; i++) {
+            if (menor > array[i]) {
+                menor = array[i];
+            }
+        }
+
+        return menor;
+    }
+
+    function max(array) {
+        let mayor = array[0];
+
+        for (let i = 0; i < array.length; i++) {
+            if (mayor < array[i]) {
+                mayor = array[i];
+            }
+        }
+        return mayor;
+    }
+
+    return {
+        average: average(array),
+        min: min(array),
+        max: max(array),
+        length: array.length,
+    };
+}
